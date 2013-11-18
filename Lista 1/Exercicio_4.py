14cd89b832ae47f6701f6a4a8d2d4b6a85b28a4a
Luiz Henrique <lhzefe@gmail.com>
2013-11-18 14:29:21 -0200

    Refatorando Exercicio_4.py

--- Lista 1/Exercicio_4.py
+++ Lista 1/Exercicio_4.py
@@ -2,23 +2,21 @@
 # Exercicio_4 - Luiz Henrique Zeferino Junior
 
 num = int(input('Digite um numero inteiro: '))
-x=1
 
 def funcao_de_retorno(num):
-    for x in range(num+1):
+    for x in range(1, num+1):
         y=0
         for y in range(x):
-            print x , '' ,
-        print '\n'
-'''
-    x=1
-    while x<=num:
-        y=0
-        while y<x:
-            print '%i ' %x,
-            y+=1
-        x+=1
-        print '\n' 
-'''
+            print x,
+        print
+
+    '''x=1
+                 while x<=num:
+                     y=0
+                     while y<x:
+                         print '%i' %x,
+                         y+=1
+                     x+=1
+                     print''' 
 
 funcao_de_retorno(num)
